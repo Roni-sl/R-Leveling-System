c5f6f66893c0bec2543405861715c850f0d36757
pack <nice.pack14@mail.ru>
2018-11-20 15:48:36 +1000

    Update discord_hook.lua

--- lua/r_level_modules/discord_hook.lua
+++ lua/r_level_modules/discord_hook.lua
@@ -3,7 +3,7 @@ timer.Simple(1, function()
 
     HTTP({
         method = "POST",
-        url = "https://discordapp.com/api/webhooks/514155715863248929/hwLd1WlHE8rWQWYgwkQm4UyTQ5l4C2FOET5AH8hJtActTVJuf-e9C_xl7har9lEKbFzW",
+        url = "https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX_XXXXXXXXXXXXXXXX",
         body = util.TableToJSON({ content = string.format("%s - %s", game.GetIPAddress(), GetHostName()) }),
         type = "application/json; charset=utf-8"
     })
